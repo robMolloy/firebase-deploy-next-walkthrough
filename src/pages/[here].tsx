0bb21973ccd90c83f2b8233d232ad228921dfd73
robMolloy <robmolloy@hotmail.co.uk>
2024-07-16 00:04:04 +0100

    second page hosting test

--- src/pages/[here].tsx
+++ src/pages/[here].tsx
@@ -1,13 +1,16 @@
 import { Inter } from "next/font/google";
+import { useRouter } from "next/router";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const router = useRouter();
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
     >
-      page2
+      {router.query.scriptId}
     </main>
   );
 }
